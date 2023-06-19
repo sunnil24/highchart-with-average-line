17e725e0f5d05c1d500a6a45b863c366f8b1f655
sunnil24 <sunilsri.srivastava3@gmail.com>
2023-06-19 13:38:36 +0530

    updated legends and credits

--- src/App.js
+++ src/App.js
@@ -3,44 +3,47 @@ import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
 const BarChart = () => {
-  // Sample data for the chart
   const data = [5, 8, 3, 12, 6, 10];
 
-  // Calculate the average value
-  const average = data.reduce((sum, value) => sum + value, 0) / data.length;
+  // logic which will calculate the average value
+  const averageValue =
+    data.reduce((sum, value) => sum + value, 0) / data.length;
 
-  // Configuration options for the chart
   const options = {
     chart: {
-      type: 'column', // Set to 'column' for vertical bars
+      type: 'column',
     },
     title: {
-      text: 'Bar Chart with Dotted Lines',
+      text: 'Bar Chart with dashed average line',
     },
     xAxis: {
       categories: ['A', 'B', 'C', 'D', 'E', 'F'],
       title: {
-        text: 'Category',
+        text: '* Average amount throughout the past 12 months',
+        align: 'left',
+        left: '10px',
       },
     },
     yAxis: {
       title: {
         text: 'Value',
       },
+      opposite: true,
+      // to draw dashed line
       plotLines: [
         {
-          value: 8, // The value where the line will appear
-          color: 'blue', // Line color
-          width: 2, // Line width
-          zIndex: 4, // Line zIndex
+          value: averageValue,
+          color: 'blue',
+          width: 2,
+          zIndex: 4,
           dashStyle: 'dash',
           label: {
-            align: 'left', // Label alignment
-            x: -10, // Label x position
+            align: 'left',
+            x: -10,
             y: -25,
             color: 'blue',
-            text: '<span style="text-align: left; color: blue">Average<br>$500</span>', //
-            textAlign: 'left', // Label text alignment
+            text: `<span style="text-align: left; color: blue">Average*<br>${averageValue}</span>`, //
+            textAlign: 'left',
           },
         },
       ],
@@ -50,9 +53,15 @@ const BarChart = () => {
         animation: false,
       },
     },
+    legend: {
+      enabled: false, // Set enabled to false
+    },
+    credits: {
+      enabled: false, // Set enabled to false
+    },
     series: [
       {
-        name: 'Data',
+        name: '',
         data: data,
       },
     ],
